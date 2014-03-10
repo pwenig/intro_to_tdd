ee148d1d3d6fb72136484f860b29778c023e7788
Paul Wenig <pwenig@gmail.com>
2014-03-10 14:00:53 -0600

    Fix test description

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,7 +3,7 @@ require 'rspec/core'
 require 'fizz_buzz'
 
 describe FizzBuzz do
-  it "passes" do
+  it "returns an array from 1 to number passed in" do
     fizzbuzz = FizzBuzz.new(5)
 
     expected = [1, 2, 3, 4, 5]
