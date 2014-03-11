ef196da021d6227bce877c495c93b7750966a531
Paul Wenig <pwenig@gmail.com>
2014-03-11 14:29:41 -0600

    passes all three critera

--- lib/leap_year_calc.rb
+++ lib/leap_year_calc.rb
@@ -1,10 +1,18 @@
 class Leap_Year
 
+
   def yes?(num)
-    num % 4 == 0
+    if num % 4 == 0
+     if  (num % 400 != 0) && (num % 100 == 0)
+       return false
+     else
+       return true
+     end
+    else
+      return false
+    end
 
 
   end
 
 end
-
