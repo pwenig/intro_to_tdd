29334ab842b1808cfd12606b24de9f723f60489c
Paul Wenig <pwenig@gmail.com>
2014-03-11 15:43:20 -0600

    final test. converts string to int and adds

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -1,12 +1,24 @@
 class String_Calc
 
+  def initialize(delimiter)
+    @delimiter = delimiter
+  end
+
+
  def add(string)
    if string == ""
      return 0
-     
+   else
+     array = string.split(@delimiter)
+     sum = 0
+     array.each do |y|
+       sum += y.to_i
+     end
+     sum
    end
-
-
  end
 
 end
+
+
+
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,12 +1,23 @@
-require "string_calc"
+require "String_Calc"
 
 describe String_Calc do
 
 it "create a add method that returns 0 when empty string is passed" do
 
-  string_calc = String_Calc.new
+  calc = String_Calc.new(",")
   expected_value = 0
-  actual_value = string_calc.add("")
+  actual_value = calc.add("")
+
+  expect(actual_value).to eq expected_value
+
+
+end
+
+it "create a add method that returns the sum of the string" do
+
+  calc = String_Calc.new(",")
+  expected_value = 10
+  actual_value = calc.add("1, 2, 7")
 
   expect(actual_value).to eq expected_value
 
