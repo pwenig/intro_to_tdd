378ac3780cdc69001c1bc4fbcfc8f68f67612cbc
Paul Wenig <pwenig@gmail.com>
2014-03-11 14:44:23 -0600

    first test. return 0 if empty string

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -1,4 +1,12 @@
 class String_Calc
 
-  
+ def add(string)
+   if string == ""
+     return 0
+     
+   end
+
+
+ end
+
 end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -2,6 +2,15 @@ require "string_calc"
 
 describe String_Calc do
 
+it "create a add method that returns 0 when empty string is passed" do
 
+  string_calc = String_Calc.new
+  expected_value = 0
+  actual_value = string_calc.add("")
+
+  expect(actual_value).to eq expected_value
+
+
+end
 
 end
