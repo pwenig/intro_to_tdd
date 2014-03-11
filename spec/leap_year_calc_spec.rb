d8aceb49d273433934bd064fbb4169e63c0976a6
Paul Wenig <pwenig@gmail.com>
2014-03-11 14:30:20 -0600

    created tests for all three criteria

--- spec/leap_year_calc_spec.rb
+++ spec/leap_year_calc_spec.rb
@@ -2,7 +2,7 @@ require "leap_year_calc"
 
 describe Leap_Year do
 
-  it "can call a method yes? that will tell you if it is a leap year" do
+  it "can call a method yes? that will tell you if it is a leap year and divisible by 4" do
 
 
     leap_year = Leap_Year.new
@@ -12,4 +12,38 @@ describe Leap_Year do
     expect(actual_value).to eq expected_value
 
   end
+
+  it "the year is a leap year if divisible by 400 and 100. Not a leap year if divisible by only 100" do
+
+
+
+    leap_year = Leap_Year.new
+    expected_value = false
+    actual_value = leap_year.yes?(1900)
+    expect(actual_value).to eq expected_value
+
+  end
+
+  it "the year is a leap year if divisible by 400 and 100. Not a leap year if divisible by only 100" do
+
+
+
+    leap_year = Leap_Year.new
+    expected_value = true
+    actual_value = leap_year.yes?(2000)
+    expect(actual_value).to eq expected_value
+
+  end
+
+  it "the year is a leap year if divisible by 400 and 100. Not a leap year if divisible by only 100" do
+
+
+
+    leap_year = Leap_Year.new
+    expected_value = false
+    actual_value = leap_year.yes?(2001)
+    expect(actual_value).to eq expected_value
+
+  end
+
 end
