3066eadbdc8f74ee0fd5ef5855e37f734a5ddd2c
Paul Wenig <pwenig@gmail.com>
2014-03-11 22:18:51 -0600

    added subtrct, save and get methods

--- lib/num_calc.rb
+++ lib/num_calc.rb
@@ -1,15 +1,31 @@
 class Num_Calc
 
   def intialize
+    @output = 0
   end
 
-  def add(number1, number2)
-    sum = number1 + number2
+  def add(num1, num2)
+    sum = num1 + num2
     @output = sum
-    return @output
 
   end
 
+  def subtract(num1, num2)
+    diff = num1 - num2
+    @output = diff
 
+  end
+
+  def save(num)
+    @output = num
+  end
+
+  def get
+    @output
+  end
+
+  def clear(num)
+    @output = 0
+  end
 end
 
--- spec/num_calc_spec.rb
+++ spec/num_calc_spec.rb
@@ -1,21 +1,35 @@
-require "Num_Calc"
+require "num_calc"
 
 describe Num_Calc do
 
   it "create an add method that takes two numbers and returns the sum of the two numbers." do
 
   num_calc = Num_Calc.new
-    expected_value = 20
-    actual_value = num_calc.add(10,10)
+  actual_value = num_calc.add(10,10)
+  expected_value = 20
     expect(actual_value).to eq expected_value
 
   end
 
 
-  it "create an subtract method that takes two numbers and returns the difference of the two numbers."
+  it "create an subtract method that takes two numbers and returns the difference of the two numbers." do
+    num_calc = Num_Calc.new
+    actual_value = num_calc.subtract(100,50)
+    expected_value = 50
+    expect(actual_value).to eq expected_value
+
+  end
 
 
-  it "create an save method that allows you to save to memory and be retrieved with a get method"
+  it "create an save method that allows you to save to memory and be retrieved with a get method" do
+    num_calc = Num_Calc.new
+    num_calc.save(50)
+
+    actual_value = num_calc.get
+    expected_value = 50
+    expect(actual_value).to eq expected_value
+
+  end
 
   it "create a clear method that clears the calculator's memory. When the memory is empty, get returns 0."
 
