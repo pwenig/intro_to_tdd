c10d842d25a8c9658786fe67acad4c65689386ca
Paul Wenig <pwenig@gmail.com>
2014-03-15 12:27:34 -0600

    returns all logs after log method has been called five times

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,9 +1,16 @@
 class Logger
 def initialize(file)
   @file = file
+  @log_storage = []
+  @count = 0
 end
 def log(message)
   @message = message
+  @log_storage << @message
+  @count += 1
+  if @count == 5
+    @log_storage.join("\n")
+  end
 
 end
 end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -12,4 +12,25 @@ describe Logger do
     logger = Logger.new(file)
     logger.log("I'm logging something")
   end
+
+  it "prints message after five logs happen" do
+    file = "Logger.txt"
+    logger = Logger.new(file)
+    logger.log("I'm logging something once")
+    logger.log("I'm logging something twice")
+    logger.log("I'm logging something three")
+    logger.log("I'm logging something four")
+
+
+    expected = "I'm logging something once
+I'm logging something twice
+I'm logging something three
+I'm logging something four
+I'm logging something five"
+
+    actual =logger.log("I'm logging something five")
+
+    expect(actual).to eq expected
+
+  end
 end
